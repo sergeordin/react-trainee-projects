647fd8c8ccd19a45b7ed09a645d03afdb5992762
Sergei Ordin <seryogaordin@gmail.com>
2023-04-29 18:47:00 +0300

    inputs

---
+++ 16-react-fragment/src/components/Card.js
@@ -0,0 +1,11 @@
+const Card = () => {
+    return (
+        <>
+            <h1>Name</h1>
+            <h3>Description</h3>
+            <button>Like!</button>
+        </>
+    );
+};
+
+export default Card;
---
+++ 18-controlled-input/src/components/Login.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+function Login() {
+    const [data, setData] = useState({ username: '', password: '' });
+
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        console.log(data);
+        alert(JSON.stringify(data));
+    }
+
+    function handleInputChange(text, name) {
+        setData({ ...data, [name]: text });
+    }
+
+    return (
+        <>
+            <h1>Login Form</h1>
+            <form onSubmit={handleFormSubmit}>
+                <label>
+                    Username:
+                    <input
+                        type="text"
+                        value={data.username}
+                        onChange={(e) => {
+                            handleInputChange(e.target.value, 'username');
+                        }}
+                    />
+                </label>
+                <label>
+                    Password:
+                    <input
+                        type="password"
+                        value={data.password}
+                        onChange={(e) => {
+                            handleInputChange(e.target.value, 'password');
+                        }}
+                    />
+                </label>
+                <button type="submit">Login</button>
+            </form>
+        </>
+    );
+}
+
+export default Login;
