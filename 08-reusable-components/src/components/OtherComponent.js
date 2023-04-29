cd95ea3f3acaa6dde1c52a484feb2baa977d9fb0
Sergei Ordin <seryogaordin@gmail.com>
2023-04-29 12:12:12 +0300

    8-9-10

---
+++ 08-reusable-components/src/components/OtherComponent.js
@@ -0,0 +1,10 @@
+function OtherComponent() {
+    return (
+        <div>
+            {' '}
+            <p>Hello from React</p>
+        </div>
+    );
+}
+
+export default OtherComponent;
---
+++ 10-conditional-rendering/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import PetInfo from './components/PetInfo';
+
+function App() {
+    return (
+        <div className="App">
+            <PetInfo animal="dog" age={4} />
+            <PetInfo animal="cat" age={7} />
+            <PetInfo hasPet={false} />
+        </div>
+    );
+}
+
+export default App;
