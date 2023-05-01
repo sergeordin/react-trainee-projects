88ec23b02c8cb3b2ffcc2134b88d1df9dc98cce4
Sergei Ordin <seryogaordin@gmail.com>
2023-05-01 20:01:50 +0300

    26 React Router

---
+++ 26-react-router/src/data/courses.js
@@ -0,0 +1,29 @@
+const courses = [
+    {
+        title: 'Complete React Guide',
+        slug: 'react',
+        id: 257,
+    },
+    {
+        title: 'Complete JavaScript Guide',
+        slug: 'javascript',
+        id: 573,
+    },
+    {
+        title: 'Complete Python Guide',
+        slug: 'python',
+        id: 872,
+    },
+    {
+        title: 'Complete Node.js Guide',
+        slug: 'node-js',
+        id: 361,
+    },
+    {
+        title: 'Complete TypeScript Guide',
+        slug: 'typescript',
+        id: 148,
+    },
+];
+
+export default courses;
